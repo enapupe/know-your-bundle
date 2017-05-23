a77e46f9abff4554a29c69377e516bad43fab215
enapupe <enapupe@gmail.com>
2017-05-23 15:34:46 -0300

    fix(constants): Change NPM API proxy ADDR

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,4 +2,4 @@ export const GITHUB_BASE_URL = 'https://github.com/'
 export const NPM_BASE_URL = 'https://www.npmjs.com/packages/'
 
 export const GITHUB_API = 'https://api.github.com/'
-export const NPM_API = 'http://138.197.212.13/'
+export const NPM_API = 'https://npm.algumacoisa.org/'
